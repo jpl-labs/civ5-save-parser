a804cdd77fdf29d379f06f55ca8f408b29bfc5c1
Matt Schmidt <matt.schmidt@omniresources.com>
2016-09-09 17:45:09 -0500

    add type/status, civ names, leader names, player colors, password and barbarian count

--- parser.js
+++ parser.js
@@ -20,27 +20,77 @@ const fs = require('fs');
             endIndex: 0
           };
 
+          result.civilizations = [];
           while (null !== (chunk = getChunk(buffer, chunk.endIndex))) {
-            // All civs are in the sixth chunk
-            if (chunkCount === 6){
-              let buffer = {
-                buffer: chunk.buffer,
-                pos: 0
-              };
-
-              console.log(readString(buffer));
+            // 2nd chunk contains the type/status of civilization - 1 alive, 2 dead, 3 human, 4 missing
+            if (chunkCount === 2){
+              while(chunk.pos < chunk.buffer.length){
+                result.civilizations.push({
+                  name: "",
+                  leader: "",
+                  type: readInt(chunk)
+                });
+              }
             }
+
+            // 6th chunk contains all civ names 
+            if (chunkCount === 6){
+              let i = 0;
+              while(chunk.pos < chunk.buffer.length){
+                let civ = readString(chunk);
+                if(civ.trim() !== ''){
+                  result.civilizations[i].name = civ;
+                }
+                i++;
+              }
+            }
+
+            // 7th chunk contains leader names and current player byte
             // The current player byte is at the end of the seventh chunk...
-            else if (chunkCount === 7) {
+            if (chunkCount === 7) {
+              // Read through leader names
+              result.barbarianCount = 0;
+              let i = 0;
+              while(chunk.pos < chunk.buffer.length && i < result.civilizations.length){
+                result.civilizations[i].leader = readString(chunk);
+                if(result.civilizations[i].leader === 'LEADER_BARBARIAN'){
+                  result.barbarianCount++;
+                }
+                i++;
+              }
               // Look backwards from end of chunk for the current player...
-              for (let i = chunk.buffer.length - 1; i >= 0; i--) {
+              for (i = chunk.buffer.length - 1; i >= 0; i--) {
                 if (chunk.buffer[i] !== 0) {
                   result.player = chunk.buffer[i];
                   break;
                 }
               }
             }
+
+            // 11th chunk contains password
+            if (chunkCount === 11){
+              //not sure whats in the first 8 bytes
+              skipBytes(chunk, 8);
+              result.password = readString(chunk);
+            }
+
+            // 23rd chunk contains player colors
+            if (chunkCount === 23){
+              // Read through player colors
+              let i = 0;
+              while(chunk.pos < chunk.buffer.length && i < result.civilizations.length){
+                result.civilizations[i].color = readString(chunk);
+                i++;
+              }
+            }
             chunkCount++;
+          }
+
+          //remove missing civs (status 4)
+          for(let i = result.civilizations.length-1; i >= 0; i--) {
+            if(result.civilizations[i].name === '' || result.civilizations[i].type == 4) {
+              result.civilizations.splice(i, 1);
+            }
           }
 
           fulfill(result);
@@ -65,7 +115,8 @@ const fs = require('fs');
 function getChunk(buffer, startIndex) {
   const delimiter = new Buffer([0x40, 0, 0, 0]);
   const result = {
-    startIndex: startIndex
+    startIndex: startIndex,
+    pos: 0
   };
 
   if (!startIndex) {
@@ -96,7 +147,7 @@ function processHeader(buffer, result){
   result.build = readString(buf);
   result.turn = readInt(buf);
   //TODO: investigate this Byte
-  buf.pos++;
+  skipBytes(buf, 1);
   result.startingCiv = readString(buf);
   result.handicap = readString(buf);
   result.era = readString(buf);
@@ -110,6 +161,8 @@ function readString(buf, length){
   let result = [];
   if(!length){
     length = readInt(buf);
+    if(length == 0)
+      return '';
   }
   
   for(let i=0; i<length; i++){
@@ -125,4 +178,8 @@ function readInt(buf){
   let int = buf.buffer.readUInt32LE(buf.pos);
   buf.pos+=4;
   return int;
+}
+
+function skipBytes(buf, num){
+  buf.pos += num;
 }
